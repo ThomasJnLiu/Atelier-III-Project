7bc8c47b22150a4871e79a8176ef0d0476a30944
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2020-09-23 13:57:36 -0400

    Painting with rays
    players are free to paint but the ray has no limit
    Should be adjusted later

---
+++ Assets/Paint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Paint : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject paintBlob;
+    public GameObject Plane;
+    public float blobSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera= this.GetComponentInChildren<Camera>();
+        Plane = GameObject.Find("Plane");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var Ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(Ray, out hit))
+            {
+                var go = PhotonNetwork.Instantiate("paintBlob", hit.point + Vector3.up * 0.1f, Quaternion.identity);
+                go.transform.localScale = Vector3.one * blobSize;
+            }
+        }
+    }
+}
