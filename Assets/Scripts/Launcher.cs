dc22641d5d412622bd08ec0967a4000eb3bd2136
Thomas <thomasjnliu@gmail.com>
2020-10-25 18:12:36 -0400

    Fix mouse lock, toggle with TAB

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -8,115 +8,132 @@ using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    public static Launcher Instance;
+  public static Launcher Instance;
 
-    void Awake(){
-        Instance = this;
-    }
-    [SerializeField] TMP_InputField roomNameInputField;
-    [SerializeField] TMP_Text errorText;
-    [SerializeField] TMP_Text roomNameText;
-    [SerializeField] Transform roomListContent;
-    [SerializeField] GameObject roomListItemPrefab;
-    [SerializeField] Transform playerListContent;
-    [SerializeField] GameObject playerListItemPrefab;
-    [SerializeField] GameObject startGameButton;
+  void Awake()
+  {
+    Instance = this;
+  }
+  [SerializeField] TMP_InputField roomNameInputField;
+  [SerializeField] TMP_Text errorText;
+  [SerializeField] TMP_Text roomNameText;
+  [SerializeField] Transform roomListContent;
+  [SerializeField] GameObject roomListItemPrefab;
+  [SerializeField] Transform playerListContent;
+  [SerializeField] GameObject playerListItemPrefab;
+  [SerializeField] GameObject startGameButton;
 
+  // Start is called before the first frame update
+  void Start()
+  {
+    Debug.Log("Connecting to Master");
 
-    // Start is called before the first frame update
-    void Start()
+    PhotonNetwork.ConnectUsingSettings();
+  }
+
+  public override void OnConnectedToMaster()
+  {
+    Debug.Log("Connected to Master");
+
+    PhotonNetwork.JoinLobby();
+    PhotonNetwork.AutomaticallySyncScene = true;
+  }
+
+  public override void OnJoinedLobby()
+  {
+    MenuManager.Instance.OpenMenu("Title");
+    Debug.Log("Joined Lobby");
+    PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
+  }
+
+  public void CreateRoom()
+  {
+    if (string.IsNullOrEmpty(roomNameInputField.text))
     {
-        Debug.Log("Connecting to Master");
+      return;
+    }
+    MenuManager.Instance.OpenMenu("Loading");
+    PhotonNetwork.CreateRoom(roomNameInputField.text);
+  }
 
-        PhotonNetwork.ConnectUsingSettings();
+  public override void OnJoinedRoom()
+  {
+    Debug.Log("joined Room " + PhotonNetwork.CurrentRoom.Name);
+    MenuManager.Instance.OpenMenu("Room");
+    roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+    Player[] players = PhotonNetwork.PlayerList;
+
+    //delete all player list items
+    foreach (Transform child in playerListContent)
+    {
+      Destroy(child.gameObject);
     }
 
-    public override void OnConnectedToMaster(){
-        Debug.Log("Connected to Master");
-
-        PhotonNetwork.JoinLobby();
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
-    
-    public override void OnJoinedLobby(){
-        MenuManager.Instance.OpenMenu("Title");
-        Debug.Log("Joined Lobby");
-        PhotonNetwork.NickName = "Player " + Random.Range(0,1000).ToString("0000");
+    for (int i = 0; i < players.Count(); i++)
+    {
+      Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
     }
 
-    public void CreateRoom(){
-        if(string.IsNullOrEmpty(roomNameInputField.text)){
-            return;
-        }
-        MenuManager.Instance.OpenMenu("Loading");
-        PhotonNetwork.CreateRoom(roomNameInputField.text);
+    startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+  }
+
+  public override void OnMasterClientSwitched(Player newMasterClient)
+  {
+    startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+  }
+
+  public override void OnCreateRoomFailed(short returncode, string message)
+  {
+    MenuManager.Instance.OpenMenu("Error");
+    errorText.text = "Room Creation Failed " + message;
+
+  }
+
+  public void LeaveRoom()
+  {
+    PhotonNetwork.LeaveRoom();
+    MenuManager.Instance.OpenMenu("Loading");
+
+  }
+
+  public override void OnLeftRoom()
+  {
+    MenuManager.Instance.OpenMenu("Title");
+  }
+
+  public override void OnRoomListUpdate(List<RoomInfo> roomList)
+  {
+    foreach (Transform trans in roomListContent)
+    {
+      Destroy(trans.gameObject);
     }
-
-    public override void OnJoinedRoom(){
-        Debug.Log("joined Room " + PhotonNetwork.CurrentRoom.Name);
-        MenuManager.Instance.OpenMenu("Room");  
-        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
-
-        Player[] players = PhotonNetwork.PlayerList;
-
-        //delete all player list items
-        foreach(Transform child in playerListContent){
-            Destroy(child.gameObject);
-        }
-
-        for (int i = 0; i < players.Count(); i++){
-            Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
-        }
-
-        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    for (int i = 0; i < roomList.Count; i++)
+    {
+      if (roomList[i].RemovedFromList)
+      {
+        continue;
+      }
+      Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
     }
+  }
 
-    public override void OnMasterClientSwitched(Player newMasterClient){
-        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
-    }
+  public void JoinRoom(RoomInfo info)
+  {
+    PhotonNetwork.JoinRoom(info.Name);
+    MenuManager.Instance.OpenMenu("Loading");
+  }
 
-    public override void OnCreateRoomFailed(short returncode, string message){
-            MenuManager.Instance.OpenMenu("Error");  
-            errorText.text = "Room Creation Failed " + message;
+  public override void OnPlayerEnteredRoom(Player newPlayer)
+  {
+    Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
 
-    }
+  }
 
-    public void LeaveRoom(){
-        PhotonNetwork.LeaveRoom();
-        MenuManager.Instance.OpenMenu("Loading");
+  public void StartGame()
+  {
+    PhotonNetwork.LoadLevel(1);
 
-    }
+  }
 
-    public override void OnLeftRoom(){
-        MenuManager.Instance.OpenMenu("Title");
-    }
-
-    public override void OnRoomListUpdate(List<RoomInfo> roomList){
-        foreach(Transform trans in roomListContent){
-            Destroy(trans.gameObject);
-        }
-        for(int i = 0; i < roomList.Count; i++){            
-            if(roomList[i].RemovedFromList){
-                continue;
-            }
-            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
-        }
-    }
-
-    public void JoinRoom(RoomInfo info){
-        PhotonNetwork.JoinRoom(info.Name);
-        MenuManager.Instance.OpenMenu("Loading");
-    }
-
-    public override void OnPlayerEnteredRoom(Player newPlayer){
-        Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
-
-    }
-
-    public void StartGame(){
-        PhotonNetwork.LoadLevel(1);
-        // Lock cursor
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-    
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,105 +5,119 @@ using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] GameObject cameraHolder;
-    [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+  [SerializeField] GameObject cameraHolder;
+  [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
 
-    [SerializeField] float verticalLookRotation;
-    [SerializeField] bool grounded = true;
+  [SerializeField] float verticalLookRotation;
+  [SerializeField] bool grounded = true;
 
-    Vector3 smoothMoveVelocity;
-    Vector3 moveAmount;
-    Rigidbody rb;
+  Vector3 smoothMoveVelocity;
+  Vector3 moveAmount;
+  Rigidbody rb;
 
-    PhotonView PV;
+  PhotonView PV;
 
-    public Animator anim;
+  public Animator anim;
 
-    void Awake(){
-        rb = GetComponent<Rigidbody>();
-        PV = GetComponent<PhotonView>();
-    }
-    // Start is called before the first frame update
-    void Start()
+  void Awake()
+  {
+    rb = GetComponent<Rigidbody>();
+    PV = GetComponent<PhotonView>();
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (!PV.IsMine)
     {
-        if(!PV.IsMine){
-            Destroy(GetComponentInChildren<Camera>().gameObject);
-            Destroy(rb);
-        }else{
-            gameObject.tag = "Player";
-        }
-
+      Destroy(GetComponentInChildren<Camera>().gameObject);
+      Destroy(rb);
     }
-
-    // Update is called once per frame
-    void Update()
+    else
     {
-        if(!PV.IsMine){
-            return;
-        }
-        // Only Look with cursor if cursor is locked
-        if(Cursor.lockState == CursorLockMode.Locked){
-            Look();
-        }
-
-        Vector3 moveDir = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
-
-        if(Input.GetKeyDown(KeyCode.Space) && grounded){
-            rb.AddForce(transform.up * jumpForce);
-            //PhotonNetwork.Instantiate("Cube", transform.position, transform.rotation, 0);
-        }
-
-
-        // Toggle cursor lock 
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            if(Cursor.lockState == CursorLockMode.Locked){
-            Cursor.lockState = CursorLockMode.None;
-            }else{
-                Cursor.lockState = CursorLockMode.Locked;
-            }
-            
-        }
+      gameObject.tag = "Player";
     }
 
-    void Look(){
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+  }
 
-        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
-
-        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+  // Update is called once per frame
+  void Update()
+  {
+    if (!PV.IsMine)
+    {
+      return;
+    }
+    // // Only Look with cursor if cursor is locked
+    if (Cursor.lockState == CursorLockMode.Locked)
+    {
+      Look();
     }
 
-    void FixedUpdate(){
-        if(!PV.IsMine){
-            return;
-        }
-        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
-        
-            if (Input.GetAxisRaw("Horizontal")!=0 || Input.GetAxisRaw("Vertical")!=0) 
-            {
-                anim.SetBool("idle", false);
-                
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                anim.SetFloat("movementSpeed",3);
-            }
-            else
-            {
-                anim.SetFloat("movementSpeed", 1);
-            }
-            }
-            else
-            {
-                anim.SetBool("idle", true);
-            }
-            
-        
+    Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+    moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+
+    if (Input.GetKeyDown(KeyCode.Space) && grounded)
+    {
+      rb.AddForce(transform.up * jumpForce);
+      //PhotonNetwork.Instantiate("Cube", transform.position, transform.rotation, 0);
     }
 
-    public void SetGroundedState(bool _grounded){
-        grounded = _grounded;
+
+    // Toggle cursor lock 
+    if (Input.GetKeyDown(KeyCode.Tab))
+    {
+      if (Cursor.lockState == CursorLockMode.Locked)
+      {
+        Cursor.lockState = CursorLockMode.None;
+      }
+      else
+      {
+        Cursor.lockState = CursorLockMode.Locked;
+      }
     }
+  }
+
+  void Look()
+  {
+    transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+    verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+    verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+    cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+  }
+
+  void FixedUpdate()
+  {
+    if (!PV.IsMine)
+    {
+      return;
+    }
+    rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+
+    if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+    {
+      anim.SetBool("idle", false);
+
+      if (Input.GetKey(KeyCode.LeftShift))
+      {
+        anim.SetFloat("movementSpeed", 3);
+      }
+      else
+      {
+        anim.SetFloat("movementSpeed", 1);
+      }
+    }
+    else
+    {
+      anim.SetBool("idle", true);
+    }
+
+
+  }
+
+  public void SetGroundedState(bool _grounded)
+  {
+    grounded = _grounded;
+  }
 }
