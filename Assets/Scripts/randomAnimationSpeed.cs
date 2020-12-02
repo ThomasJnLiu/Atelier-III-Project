973a28328e22f2def2889907d01ce9dbf01bedfc
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2020-12-01 22:54:45 -0500

    drones
    added some floating parkour obstacles

---
+++ Assets/Scripts/randomAnimationSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomAnimationSpeed : MonoBehaviour
+{
+    // Start is called before the first frame update
+     Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = Random.Range(0.3f,1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
